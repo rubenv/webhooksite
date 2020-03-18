563b28303cae38df0693a512f7f05bb9b90b7d82
Ruben Vermeersch <ruben@rocketeer.be>
2020-03-18 11:41:19 +0100

    Remove assert dependency

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/rubenv/webhooksite
 
 go 1.13
-
-require (
-	github.com/kr/pretty v0.2.0
-	github.com/stretchr/testify v1.5.1
-)
--- webhooksite_test.go
+++ webhooksite_test.go
@@ -4,36 +4,54 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
+
+func notNil(t *testing.T, v interface{}) {
+	if v == nil {
+		t.Fail()
+	}
+}
+
+func noError(t *testing.T, err error) {
+	if err != nil {
+		t.Logf("Unexpected error: %s", err)
+		t.Fail()
+	}
+}
+
+func hasLen(t *testing.T, coll []*Request, count int) {
+	if len(coll) != count {
+		t.Logf("Unexpected length: %d != %d", len(coll), count)
+		t.Fail()
+	}
+}
 
 func TestWebhooks(t *testing.T) {
 	t.Parallel()
 
-	assert := assert.New(t)
-
 	c := New()
-	assert.NotNil(c)
+	notNil(t, c)
 
 	token, err := c.CreateToken()
-	assert.NoError(err)
-	assert.NotNil(token)
+	noError(t, err)
+	notNil(t, token)
 
 	reqs, err := c.GetRequests(token.UUID)
-	assert.NoError(err)
-	assert.NotNil(reqs)
-	assert.Len(reqs.Data, 0)
+	noError(t, err)
+	notNil(t, reqs)
+	hasLen(t, reqs.Data, 0)
 
 	// Do a webhook call
 	resp, err := http.Get(c.url(fmt.Sprintf("/%s", token.UUID)))
-	assert.NoError(err)
-	assert.Equal(resp.StatusCode, http.StatusOK)
+	noError(t, err)
+	if resp.StatusCode != http.StatusOK {
+		t.Fail()
+	}
 	defer resp.Body.Close()
 
 	// Should have it now
 	reqs, err = c.GetRequests(token.UUID)
-	assert.NoError(err)
-	assert.NotNil(reqs)
-	assert.Len(reqs.Data, 1)
+	noError(t, err)
+	notNil(t, reqs)
+	hasLen(t, reqs.Data, 1)
 }
